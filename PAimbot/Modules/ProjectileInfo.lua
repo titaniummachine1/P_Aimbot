7e9bf88300141fc9bd5c9a88cf8fbf7d4ca0510b
titanium_machine <78664175+titaniummachine1@users.noreply.github.com>
2024-09-14 15:11:55 +0200

    v1

--- PAimbot/Modules/ProjectileInfo.lua
+++ PAimbot/Modules/ProjectileInfo.lua
@@ -1,213 +1,267 @@
--- Import the Common module for utility functions
-local Common = require("PAimbot.Common")
-local aItemDefinitions = require("PAimbot.Modules.laDefinitions")
+--[[           ProjectileInfo Module         ]]--
+--[[        Updated to include all           ]]--
+--[[          projectile types               ]]--
 
--- Define the ProjectileInfo module
+-- Required modules or APIs
+local Common = require("PAimbot.Common")  -- Ensure this module contains the CLAMP function
+local aItemDefinitions = require("PAimbot.Modules.laDefinitions")  -- Adjust as needed
+
+-- ProjectileInfo Module
 local ProjectileInfo = {}
-ProjectileInfo.__index = ProjectileInfo
 
--- Precache static data: offsets and collision sizes
+-- Constants
+local FL_DUCKING = 2  -- Adjust based on your environment
+local UP_VECTOR = Vector3(0, 0, 1)
+
+-- Predefined offsets and collision sizes for projectiles
 local OFFSET_TABLE = {
     stickyBomb = Vector3(16, 8, -6),
     huntsman = Vector3(23.5, -8, -3),
     flareGun = Vector3(23.5, 12, -3),
-    syringeGun = Vector3(16, 6, -8)
+    syringeGun = Vector3(16, 6, -8),
+    rocketLauncher = Vector3(23.5, -8, -3),
+    jarate = Vector3(23.5, -8, -3),
+    grenadeLauncher = Vector3(16, 8, -6),
+    flamethrower = Vector3(0, 0, 0),  -- Adjust as necessary
+    energyBall = Vector3(23.5, -8, -3),
+    throwable = Vector3(23.5, -8, -3),
+    grapplingHook = Vector3(23.5, -8, -3)
 }
 
-local COLLISION_MAX_TABLE = {
+local COLLISION_SIZE_TABLE = {
     none = Vector3(0, 0, 0),
     small = Vector3(1, 1, 1),
     medium = Vector3(3.2, 3.2, 3.2),
     large = Vector3(3, 3, 3)
 }
 
--- Local function to calculate the current charge time based on when the charge began
-local function GetCurrentChargeTime(pWeapon)
-    local fChargeBeginTime = (pWeapon:GetPropFloat("m_flChargeBeginTime") or 0)
-    if fChargeBeginTime ~= 0 then
-        fChargeBeginTime = globals.CurTime() - fChargeBeginTime
-    end
-    return fChargeBeginTime
-end
-
--- Constructor for the ProjectileInfo class
-function ProjectileInfo:new()
-    self = setmetatable({}, ProjectileInfo)
-    -- Initialize default properties
-    self.pLocal = nil
-    self.bDucking = false
-    self.iCase = 0
-    self.iDefIndex = 0
-    self.iWepID = 0
-    self.fChargeBeginTime = 0
-    return self
-end
-
--- Method to initialize or update the ProjectileInfo instance with new data
-function ProjectileInfo:Update(data)
-    self.pLocal = data.pLocal
-    self.bDucking = data.bDucking
-    self.iCase = data.iCase
-    self.iDefIndex = data.iDefIndex
-    self.iWepID = data.iWepID
-    self.fChargeBeginTime = GetCurrentChargeTime(data.pWeapon)
-end
-
--- Method to destroy the ProjectileInfo instance
-function ProjectileInfo:Destroy()
-    self.pLocal = nil
-    self.bDucking = nil
-    self.iCase = nil
-    self.iDefIndex = nil
-    self.iWepID = nil
-    self.fChargeBeginTime = nil
-end
-
--- Standardized method for handling projectile information retrieval
-function ProjectileInfo:GetProjectileInfo(offsetType, forwardVelocity, upwardVelocity, collisionType, gravity, drag)
-    return {
-        OFFSET_TABLE[offsetType],
-        forwardVelocity,
-        upwardVelocity or 0,
-        COLLISION_MAX_TABLE[collisionType],
-        gravity or 0,
-        drag or 0
-    }
-end
-
--- Weapon-specific configuration functions
-local projectileConfigurations = {
-    -- Rocket Launcher Info
-    [-1] = function(self)
-        local vOffset = Vector3(23.5, -8, self.bDucking and 8 or -3)
-        local fForwardVelocity = 1200
-
-        if self.iWepID == 22 or self.iWepID == 65 then
-            vOffset.y, fForwardVelocity =
-                (self.iDefIndex == 513) and 0 or 12,
-                (self.iWepID == 65) and 2000 or (self.iDefIndex == 414) and 1550 or 1100
-        elseif self.iWepID == 109 then
-            vOffset.y, vOffset.z = 6, -3
-        end
-
-        return {
-            vOffset,
-            fForwardVelocity,
-            0,
-            COLLISION_MAX_TABLE.small,
-            0
-        }
-    end,
-
-    -- Sticky Bomb Info
-    [1] = function(self)
-        local baseVelocity = 900
-        local maxVelocityIncrease = 1500
-        local velocity = baseVelocity + Common.CLAMP(self.fChargeBeginTime / 4, 0, 1) * maxVelocityIncrease
-
-        return self:GetProjectileInfo("stickyBomb", velocity, 200, "medium")
-    end,
-
-    -- Iron Bomber Info
-    [4] = function(self)
-        return self:GetProjectileInfo("stickyBomb", 1200, 200, "medium", 400, 0.45)
-    end,
-
-    -- Grenade Launcher Info
-    [5] = function(self)
-        local forwardVelocity = (self.iDefIndex == 308) and 1500 or 1200
-        local drag = (self.iDefIndex == 308) and 0.225 or 0.45
-        return self:GetProjectileInfo("stickyBomb", forwardVelocity, 200, "medium", 400, drag)
-    end,
-
-    -- Loose Cannon Info
-    [6] = function(self)
-        return self:GetProjectileInfo("stickyBomb", 1440, 200, "medium", 560, 0.5)
-    end,
-
-    -- Huntsman Info
-    [7] = function(self)
-        local baseVelocity = 1800
-        local maxVelocityIncrease = 800
-        local velocity = baseVelocity + Common.CLAMP(self.fChargeBeginTime, 0, 1) * maxVelocityIncrease
-        local gravity = 200 - Common.CLAMP(self.fChargeBeginTime, 0, 1) * 160
-        return self:GetProjectileInfo("huntsman", velocity, 0, "small", gravity)
-    end,
-
-    -- Flare Gun Info
-    [8] = function(self)
-        return self:GetProjectileInfo("flareGun", 2000, 0, "none", 120)
-    end,
-
-    -- Crossbow Info
-    [9] = function(self)
-        local collisionType = (self.iDefIndex == 997) and "small" or "large"
-        return self:GetProjectileInfo("huntsman", 2400, 0, collisionType, 80)
-    end,
-
-    -- Syringe Gun Info
-    [10] = function(self)
-        return self:GetProjectileInfo("syringeGun", 1000, 0, "small", 120)
-    end,
-
-    -- Jarate Info
-    [11] = function(self)
-        return self:GetProjectileInfo("huntsman", 1000, 200, "large", 450)
-    end,
-
-    -- Guillotine Info
-    [12] = function(self)
-        return self:GetProjectileInfo("flareGun", 3000, 300, "medium", 900, 1.3)
-    end
+-- Projectile Types Enumeration
+local E_ProjectileType = {
+    TF_PROJECTILE_NONE = 0,
+    TF_PROJECTILE_BULLET = 1,
+    TF_PROJECTILE_ROCKET = 2,
+    TF_PROJECTILE_PIPEBOMB = 3,
+    TF_PROJECTILE_PIPEBOMB_REMOTE = 4,
+    TF_PROJECTILE_SYRINGE = 5,
+    TF_PROJECTILE_FLARE = 6,
+    TF_PROJECTILE_JAR = 7,
+    TF_PROJECTILE_ARROW = 8,
+    TF_PROJECTILE_FLAME_ROCKET = 9,
+    TF_PROJECTILE_JAR_MILK = 10,
+    TF_PROJECTILE_HEALING_BOLT = 11,
+    TF_PROJECTILE_ENERGY_BALL = 12,
+    TF_PROJECTILE_ENERGY_RING = 13,
+    TF_PROJECTILE_PIPEBOMB_PRACTICE = 14,
+    TF_PROJECTILE_CLEAVER = 15,
+    TF_PROJECTILE_STICKY_BALL = 16,
+    TF_PROJECTILE_CANNONBALL = 17,
+    TF_PROJECTILE_BUILDING_REPAIR_BOLT = 18,
+    TF_PROJECTILE_FESTIVE_ARROW = 19,
+    TF_PROJECTILE_THROWABLE = 20,
+    TF_PROJECTILE_SPELL = 21,
+    TF_PROJECTILE_FESTIVE_JAR = 22,
+    TF_PROJECTILE_FESTIVE_HEALING_BOLT = 23,
+    TF_PROJECTILE_BREADMONSTER_JARATE = 24,
+    TF_PROJECTILE_BREADMONSTER_MADMILK = 25,
+    TF_PROJECTILE_GRAPPLINGHOOK = 26,
+    TF_PROJECTILE_SENTRY_ROCKET = 27,
+    TF_PROJECTILE_BREAD_MONSTER = 28
 }
 
--- Method to retrieve the correct projectile information based on the weapon type
-function ProjectileInfo:GetProjectileInformation()
-    local configFunction = projectileConfigurations[self.iCase]
-    if configFunction then
-        return configFunction(self)
-    else
-        return nil
+-- Function to get the current charge time of a weapon
+local function GetCurrentChargeTime(weapon)
+    local chargeBeginTime = weapon:GetChargeBeginTime() or 0
+    if chargeBeginTime ~= 0 then
+        chargeBeginTime = globals.CurTime() - chargeBeginTime
     end
+    return chargeBeginTime
 end
 
--- Initialize the ProjectileInfo instance once
-local projectileInfoInstance = ProjectileInfo:new()
+-- Main function to get projectile data
+function ProjectileInfo.GetProjectileData(pLocal, weapon)
+    -- Initialize the table to return
+    local projData = {}
+    
+    -- Get weapon data
+    local weaponID = weapon:GetWeaponID()
+    local itemDefIndex = weapon:GetPropInt("m_iItemDefinitionIndex") or 0
+    local weaponData = weapon:GetWeaponData() or {}
+    local projectileType = weaponData.projectile or 0
+    local baseSpeed = weaponData.projectileSpeed or 0
+    local gravity = weapon:GetProjectileGravity() or 0
+    local drag = weapon:GetProjectileSpread() or 0
 
--- Retrieve the projectile information object
-function ProjectileInfo.GetProjectileInformationObject(pLocal, pWeapon)
-    -- Get the item definition index from the weapon
-    local iItemDefinitionIndex = pWeapon:GetPropInt("m_iItemDefinitionIndex")
+    -- Determine if player is ducking
+    local isDucking = (pLocal:GetPropInt("m_fFlags") & FL_DUCKING) == FL_DUCKING
 
-    -- Determine the type of item definition from the predefined table
-    local iItemDefinitionType = aItemDefinitions[iItemDefinitionIndex] or 0
-    if iItemDefinitionType == 0 then return nil end  -- Return nil if the item is not valid
+    -- Get charge time if applicable
+    local chargeTime = GetCurrentChargeTime(weapon)
 
-    -- Determine if the player is ducking
-    local isDucking = (pLocal:GetPropInt("m_fFlags") & FL_DUCKING) == 2
+    -- Initialize default values
+    projData.Offset = Vector3(0, 0, 0)
+    projData.ForwardVelocity = baseSpeed
+    projData.UpwardVelocity = 0
+    projData.CollisionSize = Vector3(0, 0, 0)
+    projData.Gravity = gravity
+    projData.Drag = drag
+    projData.ProjectileType = projectileType
 
-    -- Prepare the data table for updating the projectile info instance
-    local data = {
-        pLocal = pLocal,
-        bDucking = isDucking,
-        iCase = iItemDefinitionType,
-        iDefIndex = iItemDefinitionIndex,
-        iWepID = pWeapon:GetWeaponID(),
-        pWeapon = pWeapon
-    }
+    -- Handle specific projectile types
+    if projectileType == E_ProjectileType.TF_PROJECTILE_BULLET then
+        -- Hitscan weapons
+        return nil  -- Hitscan weapons don't have projectile data
 
-    -- Update the existing ProjectileInfo instance with new data
-    projectileInfoInstance:Update(data)
+    elseif projectileType == E_ProjectileType.TF_PROJECTILE_ROCKET then
+        -- Rocket Launcher
+        projData.Offset = Vector3(23.5, -8, isDucking and 8 or -3)
+        projData.ForwardVelocity = baseSpeed > 0 and baseSpeed or 1100
+        projData.CollisionSize = COLLISION_SIZE_TABLE.small
 
-    -- Retrieve the correct configuration function based on the item type
-    local configFunction = projectileConfigurations[iItemDefinitionType]
-    if configFunction then
-        -- Call the function to get the projectile information
-        return configFunction(projectileInfoInstance), iItemDefinitionType
+    elseif projectileType == E_ProjectileType.TF_PROJECTILE_PIPEBOMB or
+           projectileType == E_ProjectileType.TF_PROJECTILE_PIPEBOMB_REMOTE or
+           projectileType == E_ProjectileType.TF_PROJECTILE_PIPEBOMB_PRACTICE then
+        -- Stickybomb Launcher and practice bombs
+        projData.Offset = OFFSET_TABLE.stickyBomb
+        local chargeFactor = Common.CLAMP(chargeTime / 4, 0, 1)
+        projData.ForwardVelocity = 900 + chargeFactor * 1500
+        projData.UpwardVelocity = 200
+        projData.CollisionSize = COLLISION_SIZE_TABLE.medium
+        projData.Gravity = 400
+        projData.Drag = 0.5
+
+    elseif projectileType == E_ProjectileType.TF_PROJECTILE_SYRINGE then
+        -- Syringe Gun
+        projData.Offset = OFFSET_TABLE.syringeGun
+        projData.ForwardVelocity = 1000
+        projData.Gravity = 120
+        projData.CollisionSize = COLLISION_SIZE_TABLE.small
+
+    elseif projectileType == E_ProjectileType.TF_PROJECTILE_FLARE then
+        -- Flare Gun
+        projData.Offset = OFFSET_TABLE.flareGun
+        projData.ForwardVelocity = 2000
+        projData.Gravity = 120
+        projData.CollisionSize = COLLISION_SIZE_TABLE.none
+
+    elseif projectileType == E_ProjectileType.TF_PROJECTILE_JAR or
+           projectileType == E_ProjectileType.TF_PROJECTILE_JAR_MILK or
+           projectileType == E_ProjectileType.TF_PROJECTILE_FESTIVE_JAR or
+           projectileType == E_ProjectileType.TF_PROJECTILE_BREADMONSTER_JARATE or
+           projectileType == E_ProjectileType.TF_PROJECTILE_BREADMONSTER_MADMILK then
+        -- Jarate / Mad Milk and variants
+        projData.Offset = OFFSET_TABLE.jarate
+        projData.ForwardVelocity = 1000
+        projData.UpwardVelocity = 200
+        projData.Gravity = 450
+        projData.CollisionSize = COLLISION_SIZE_TABLE.large
+
+    elseif projectileType == E_ProjectileType.TF_PROJECTILE_ARROW or
+           projectileType == E_ProjectileType.TF_PROJECTILE_FESTIVE_ARROW then
+        -- Huntsman / Crossbow and festive variants
+        projData.Offset = OFFSET_TABLE.huntsman
+        local chargeFactor = Common.CLAMP(chargeTime, 0, 1)
+        projData.ForwardVelocity = 1800 + chargeFactor * 800
+        projData.Gravity = 200 - chargeFactor * 160
+        projData.CollisionSize = COLLISION_SIZE_TABLE.small
+
+    elseif projectileType == E_ProjectileType.TF_PROJECTILE_FLAME_ROCKET then
+        -- Flamethrower
+        projData.Offset = OFFSET_TABLE.flamethrower
+        projData.ForwardVelocity = 2300
+        projData.Gravity = 0
+        projData.CollisionSize = COLLISION_SIZE_TABLE.none
+
+    elseif projectileType == E_ProjectileType.TF_PROJECTILE_HEALING_BOLT or
+           projectileType == E_ProjectileType.TF_PROJECTILE_FESTIVE_HEALING_BOLT then
+        -- Crusader's Crossbow and festive variant
+        projData.Offset = OFFSET_TABLE.huntsman
+        projData.ForwardVelocity = 2400
+        projData.Gravity = 80
+        projData.CollisionSize = COLLISION_SIZE_TABLE.small
+
+    elseif projectileType == E_ProjectileType.TF_PROJECTILE_ENERGY_BALL then
+        -- Cow Mangler / Righteous Bison
+        projData.Offset = OFFSET_TABLE.energyBall
+        projData.ForwardVelocity = 1200
+        projData.Gravity = 0
+        projData.CollisionSize = COLLISION_SIZE_TABLE.small
+
+    elseif projectileType == E_ProjectileType.TF_PROJECTILE_CLEAVER then
+        -- Flying Guillotine
+        projData.Offset = OFFSET_TABLE.flareGun
+        projData.ForwardVelocity = 3000
+        projData.UpwardVelocity = 300
+        projData.Gravity = 900
+        projData.CollisionSize = COLLISION_SIZE_TABLE.medium
+        projData.Drag = 1.3
+
+    elseif projectileType == E_ProjectileType.TF_PROJECTILE_STICKY_BALL then
+        -- Wrap Assassin Ball
+        projData.Offset = OFFSET_TABLE.stickyBomb
+        projData.ForwardVelocity = 2000
+        projData.Gravity = 1000
+        projData.CollisionSize = COLLISION_SIZE_TABLE.small
+
+    elseif projectileType == E_ProjectileType.TF_PROJECTILE_CANNONBALL then
+        -- Loose Cannon
+        projData.Offset = OFFSET_TABLE.stickyBomb
+        projData.ForwardVelocity = 1453  -- Based on game data
+        projData.UpwardVelocity = 200
+        projData.CollisionSize = COLLISION_SIZE_TABLE.medium
+        projData.Gravity = 560
+        projData.Drag = 0.5
+
+    elseif projectileType == E_ProjectileType.TF_PROJECTILE_BUILDING_REPAIR_BOLT then
+        -- Rescue Ranger Bolt
+        projData.Offset = OFFSET_TABLE.huntsman
+        projData.ForwardVelocity = 2400
+        projData.Gravity = 0
+        projData.CollisionSize = COLLISION_SIZE_TABLE.small
+
+    elseif projectileType == E_ProjectileType.TF_PROJECTILE_THROWABLE then
+        -- Throwable items like Gas Passer
+        projData.Offset = OFFSET_TABLE.throwable
+        projData.ForwardVelocity = 1000
+        projData.UpwardVelocity = 200
+        projData.Gravity = 450
+        projData.CollisionSize = COLLISION_SIZE_TABLE.large
+
+    elseif projectileType == E_ProjectileType.TF_PROJECTILE_SPELL then
+        -- Spells from Halloween events
+        projData.Offset = OFFSET_TABLE.throwable
+        projData.ForwardVelocity = 1200
+        projData.Gravity = 400
+        projData.CollisionSize = COLLISION_SIZE_TABLE.medium
+
+    elseif projectileType == E_ProjectileType.TF_PROJECTILE_GRAPPLINGHOOK then
+        -- Grappling Hook
+        projData.Offset = OFFSET_TABLE.grapplingHook
+        projData.ForwardVelocity = 3000
+        projData.Gravity = 0
+        projData.CollisionSize = COLLISION_SIZE_TABLE.small
+
+    elseif projectileType == E_ProjectileType.TF_PROJECTILE_SENTRY_ROCKET then
+        -- Sentry Rocket
+        projData.Offset = OFFSET_TABLE.rocketLauncher
+        projData.ForwardVelocity = 1100
+        projData.CollisionSize = COLLISION_SIZE_TABLE.small
+
+    elseif projectileType == E_ProjectileType.TF_PROJECTILE_BREAD_MONSTER then
+        -- Bread Monster
+        projData.Offset = OFFSET_TABLE.throwable
+        projData.ForwardVelocity = 1000
+        projData.Gravity = 400
+        projData.CollisionSize = COLLISION_SIZE_TABLE.medium
+
+    -- Add any additional projectile types here as needed
+
     else
-        -- Return nil if no configuration function exists for the item type
+        -- Handle unknown or unsupported projectile types
         return nil
     end
+
+    -- Return the projectile data table
+    return projData
 end
 
 return ProjectileInfo
